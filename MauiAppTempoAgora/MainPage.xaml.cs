c64ef084dbe307fa41507605ced0343c0bab425b
evanslibune <eveeh.libune@gmail.com>
2025-04-25 08:47:28 -0300

    Criação do app e função de buscar via api open weather

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace MauiAppTempoAgora
+﻿using System.Threading.Tasks;
+using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +13,42 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+                    if (t != null)
+                    {
+                        string dados_previsão = "";
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                        dados_previsão = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        lbl_res.Text = dados_previsão;
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de previsão";
+                    }
+                    {
+
+                    }
+                } else 
+                { 
+                    lbl_res.Text = "Digite a cidade";
+                }
+
+            }catch(Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 
